e3a411103c86d04c8c88192067cead0756537d1a
YeWenguang <166815701+YeWenguang@users.noreply.github.com>
2024-08-03 15:52:00 +0800

    Update num_pseudocodes_in_SPoC.py

--- DatasetProcess/SPoCProcess/num_pseudocodes_in_SPoC.py
+++ DatasetProcess/SPoCProcess/num_pseudocodes_in_SPoC.py
@@ -3,36 +3,21 @@ import datetime
 import json
 import os
 
-# file_path = "/home/yewenguang/work/Code-Llama/spoc/test/spoc-testw.tsv"
-#
-# # 打开.tsv文件
-# with open(file_path, 'r') as file:
-#     count = 0
-#     # 逐行读取文件内容
-#     for line in file:
-#         # 按制表符分割每一行
-#         columns = line.strip().split('\t')
-#         # 检查倒数第二列是否为"0"
-#         if len(columns) >= 2 and columns[-2] == "10":
-#             count += 1
-#
-# print("倒数第二列中包含字符串'0'的次数为:", count)
+file_path = " "
 
-
-file_path = "/home/yewenguang/work/codellama-main/SPoC/test-testp-2.txt"
-
-# 打开.tsv文件
+# Open the .tsv file
 with open(file_path, "r", encoding="utf-8") as file:
     pseudocode_content = file.read()
 
-# 将伪代码拆分为每个probid的部分
+# Split the pseudocode into parts for each probid
 pseudocodes = pseudocode_content.split("\n\n")
 
-# 统计伪代码的数量
+# Count the number of pseudocode parts
 num_pseudocodes = len(pseudocodes)
 if not pseudocodes[-1].strip():
     num_pseudocodes -= 1
 
-print(f"文件中一共有 {num_pseudocodes} 个伪代码。")
+print(f"There are {num_pseudocodes} pseudocode parts in the file.")
+
 
 
